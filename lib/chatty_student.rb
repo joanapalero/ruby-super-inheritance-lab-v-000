d42fe6612d92354bc4f99eb867d929f9de490ca2
Joana Palero <joana.palero24@gmail.com>
2018-07-08 18:34:10 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,5 +1,5 @@
 class ChattyStudent < Student 
   def hello
     super
-    puts "How are you doing today?"
+    puts "How are you doing today? I'm okay, but kind of tired. Did you watch The Walking Dead last night? You didn't?! "
   end
